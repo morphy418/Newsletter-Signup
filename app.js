7656f61438103612a1a6a33f0f5fa82b7fd638b2
Zoltan Harshegyi <z.harshegyi@gmail.com>
2020-12-19 12:40:27 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const { post } = require('request');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  app.post('/failure', function(req, res){
+    res.redirect('/');
+  });
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/050df4b317"
+
+  const options = {
+    method: "POST",
+    auth: "zoltan1:b0a9495adebbfbd944bdf161a8b243a1-us7"
+  }
+
+const request = https.request(url, options, function(response){
+  
+  if (response.statusCode === 200) {
+    res.sendFile(__dirname + '/success.html');
+  } else {
+    res.sendFile(__dirname + '/failure.html');
+  }
+  
+  response.on('data', function(data){
+    console.log(JSON.parse(data));
+  });
+});
+  
+request.write(jsonData);
+request.end();
+
+});
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000.");
+});
+
+// API Key
+// b0a9495adebbfbd944bdf161a8b243a1-us7
+
+//List ID
+// 050df4b317
